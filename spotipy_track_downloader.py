ec3e7a43eeb91dc74ccfaa9c800675af87470f7f
Yae Miko <74785477+Sakura-nee@users.noreply.github.com>
2022-02-14 10:10:47 +0700

    Update: add metadata to mp3 file

--- spotipy_track_downloader.py
+++ spotipy_track_downloader.py
@@ -5,6 +5,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
 import sys
 import requests
+import mutagen.id3
+import mutagen.mp3
 from pprint import pprint
 from mutagen.easyid3 import EasyID3, ID3
 from mutagen.id3 import APIC as AlbumCover, USLT, COMM as Comment
@@ -23,6 +25,8 @@ class MyLogger(object):
 def my_hook(d):
 	if d['status'] == 'finished':
 		print('Done downloading, now converting ...')
+	if d['status'] == 'downloading':
+			print(d['filename'], d['_percent_str'], d['_eta_str'])
 
 
 def download_yt(url, name):
@@ -40,8 +44,26 @@ def download_yt(url, name):
 	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
 		ydl.download([url])
 
-def _set_id3_mp3(converted_file_path, url):
+def add_id3_tag(converted_file_path):
+	M = mutagen.mp3.MP3(converted_file_path)
+	if M.tags is None:
+		M.tags = mutagen.id3.ID3()
+	M.tags['TIT2'] = mutagen.id3.TIT2(encoding=1)
+	M.save(v1=0, v2_version=3)
+	return True
+
+def embed_metadata(audio_file, object_metadata):
+	audio_file.delete()
+	audio_file["title"] = object_metadata['title']
+	audio_file["artist"] = object_metadata['artist']
+	audio_file["album"] = object_metadata['album']
+	return audio_file
+
+def _set_id3_mp3(converted_file_path, url, object_metadata):
+	add_id3_tag(converted_file_path)
 	audio_file = EasyID3(converted_file_path)
+	audio_file = embed_metadata(audio_file, object_metadata)
+	audio_file.save(v2_version=3)
 	audio_file = embed_cover(audio_file, converted_file_path, url)
 	audio_file.save(v2_version=3)
 	
@@ -51,26 +73,28 @@ def embed_cover(audio_file, file, imgurl):
 	audio_file["APIC"] = AlbumCover(encoding=3, mime="image/jpeg", type=3, desc="Cover", data=rawAlbumArt)
 	return audio_file
 
-urn = 'spotify:track:3D8dMWN3O6hbIlts9FXi9Z'
+if len(sys.argv) > 1:
+	urn = sys.argv[1]
+else:
+	urn = 'spotify:track:2Le7mdwiT47oCNwFL8DDzQ'
+
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="af2a4d3485b64a0597be745b8ce5ffe2", client_secret="441a934a78844eecb438b129edb3f626"))
 
 track = sp.track(urn)
-artist = track['album']['artists']
-if len(artist) == 1 or len(artist) < 1:
-	artists_name = artist[0]['name']
-else:
-	artists_name = ''
-	for artist_name in artist:
-		name = artist_name['name']
-		artists_name += name + ', '
-	if artists_name.endswith(', '):
-		artists_name = artists_name[:-2]
+metadata_dict = {}
+
+contributing_artists = [artist["name"] for artist in track["artists"]]
+artist = ", ".join(contributing_artists)
+
+metadata_dict['title']  = track["name"]
+metadata_dict['album']  = track["album"]["name"]
+metadata_dict['artist'] = artist
 
 album_art = track['album']['images'][0]['url']
-track_name = track['name'] + ' - ' + artists_name
+track_name = track['name'] + ' - ' + artist
 
 get_youtube_info = YoutubeSearch(track_name, max_results=1).to_dict()[0]
 youtube_url = 'https://www.youtube.com' + get_youtube_info['url_suffix']
 duration = get_youtube_info['duration']
 download_yt(youtube_url, track_name)
-_set_id3_mp3(track_name + '.mp3', album_art)
+_set_id3_mp3(track_name + '.mp3', album_art, metadata_dict)
